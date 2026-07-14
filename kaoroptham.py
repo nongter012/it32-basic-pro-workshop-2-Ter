67a5366a6629822729806fef9f3ca7af7dec5651
peddeal <88126567+peddeal@users.noreply.github.com>
2026-07-14 16:32:39 +0700

    addkaorop2

--- kaoroptham.py
+++ kaoroptham.py
@@ -3,3 +3,10 @@ age = input("ผู้สมัครอายุเท่าไหร่")
 height = input("ผู้สมัครส่วนสูงเท่าไหร่")
 power = input("คะแนนพละกำลังของผุ้สมัคร")
 money = input("เงินติดตัวของผู้สมัคร")
+
+age = 20
+height = 167
+power = 8
+money = 67
+if age >= 20
+    print("Pass")
