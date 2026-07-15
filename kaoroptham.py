9a150b4c70897641aebe4c721d3bc9be088f96b0
Nititada Kaoroptham <304257638+nongter012@users.noreply.github.com>
2026-07-16 01:34:03 +0700

    kaorop2cp

--- kaoroptham.py
+++ kaoroptham.py
@@ -1,12 +1,15 @@
-name = input("กรอกชื่อผู้สมัคร")
-age = input("ผู้สมัครอายุเท่าไหร่")
-height = input("ผู้สมัครส่วนสูงเท่าไหร่")
-power = input("คะแนนพละกำลังของผุ้สมัคร")
-money = input("เงินติดตัวของผู้สมัคร")
+name = input("ชื่อผู้สมัคร: ")
+age = int(input("อายุ: "))
+height = int(input("ส่วนสูง: "))
+power = int(input("พละกำลัง: "))
+money = int(input("เงินติดตัว: "))
 
-age = 20
-height = 167
-power = 8
-money = 67
-if age >= 20
-    print("Pass")
+if age >= 15 and height >= 160 and power >= 6 and money >= 67:
+    if power >= 9 and money >= 50:
+        print(name + " ได้รับตำแหน่ง: กัปตันทีม")
+    elif power >= 7 and height >= 170:
+        print(name + " ได้รับตำแหน่ง: ผู้ช่วยกัปตันทีม")
+    elif power >= 5 and height >= 170:
+        print(name + " ได้รับตำแหน่ง: ผู้ช่วยทีม")
+else:
+    print(name + " ไม่ผ่านการคัดกรอง")
